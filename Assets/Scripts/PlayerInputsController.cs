3378fd81aa600cff3dd801128cc420e2a645f194
GrafEnters <graf.enters@gmail.com>
2024-01-21 22:08:13 +0300

    Added TMP & Fixed tech scene

--- Assets/Scripts/PlayerInputsController.cs
+++ Assets/Scripts/PlayerInputsController.cs
@@ -2,13 +2,16 @@ using System;
 using DefaultNamespace;
 using UnityEngine;
 
-public class Player : ComponentsContainer {
+public class PlayerInputsController : ComponentsContainer {
     private void Update() {
         if (Input.GetKeyDown(KeyCode.I)) {
             if (InventoryDialog.isActive) {
                 UIFactory.CloseInventory();
             } else {
                 var i = GetComponent<InventoryComponent>();
+                if (i == null) {
+                    i = GetComponentInChildren<InventoryComponent>();
+                }
                 UIFactory.ShowInventory(i.Inventory);
             }
         }
